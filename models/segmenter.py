5f1cce66d614e2879ce391a76aa7508429e98d13
Ryan J <ryan.qiyu.jiang@gmail.com>
2020-09-01 17:58:43 -0400

    f

--- models/segmenter.py
+++ models/segmenter.py
@@ -130,8 +130,8 @@ class SegModel(pl.LightningModule):
 
 class SegSeedModel(pl.LightningModule):
 
-    def __init__(self):
-        super().__init__(lr=7e-3)
+    def __init__(self, lr=7e-3):
+        super().__init__()
         self.num_classes = 9
         self.encoder = networks.ResnetEncoder(18, True)
         self.decoder = networks.DepthDecoder(
