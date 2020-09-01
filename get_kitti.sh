aae430277705a3880c4b2144a69cc8dfa0efa58f
Ryan J <ryan.qiyu.jiang@gmail.com>
2020-09-01 01:40:55 -0400

    f

--- get_kitti.sh
+++ get_kitti.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 mkdir data
 cd data
 wget https://s3.eu-central-1.amazonaws.com/avg-kitti/data_object_image_2.zip
@@ -12,7 +13,5 @@ mv training/image_2 kitti/images/train
 mv testing/image_2 kitti/images/test 
 
 mv training/label_2 kitti/labels/train 
-mv testing/label_2 kitti/labels/test 
 cd kitti/images/train && ls -d $PWD/* > ../../train.txt
-cd kitti/images/test && ls -d $PWD/* > ../../test.txt
 cd ..
