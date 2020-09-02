ae7457ad932cff33cf8b6b64185febb1b16bffa4
Ryan J <ryan.qiyu.jiang@gmail.com>
2020-09-02 00:23:42 -0400

    f

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,4 @@ from .classifier import ClassifierModel
 from .decoder import DebugDepthDecoder
 from .joint import JointModel
 from .segmenter import SegModel, SegSeedModel
+from .deeplab import DeeplabModel
