cf061482f16b49148306392b0b24970f0cc5c38e
Ryan J <ryan.qiyu.jiang@gmail.com>
2020-09-03 12:17:53 -0400

    f

--- datasets/kitti_dataset.py
+++ datasets/kitti_dataset.py
@@ -110,45 +110,41 @@ class StereoDataset(torch.utils.data.Dataset):
         self.label_files = [path.replace('images', 'labels').replace('.png', '.txt').replace('.jpg', '.txt') for path in self.img_files]
         self.max_objects = 50
 
-    def __getitem__(self, data_index):
+    def __getitem__(self, file_index):
         
-        file_index = int(data_index/2)
-        use_pair = data_index % 2 == 0
-        img_path = self.img_files[file_index % len(self.img_files)].rstrip()
-        if use_pair:
-            img_path = img_path.replace('images', 'right_images')
-        img = Image.open(img_path)
+        left_img_path = self.img_files[file_index % len(self.img_files)].rstrip()
+        right_img_path = left_img_path.replace('images', 'right_images')
+        img_left = Image.open(left_img_path)
+        img_right = Image.open(right_img_path)
+
         transform = transforms.Compose([transforms.Resize((192, 640), interpolation=2),
                                         # transforms.RandomHorizontalFlip(p=0.5), 
                                         transforms.ToTensor(), 
                                         transforms.Normalize(mean, std)])
-        input_img = transform(img)
+        input_img_left = transform(img_left)
+        input_img_right = transform(img_right)
 
-        if not use_pair:
-            label_path = self.label_files[file_index % len(self.img_files)].rstrip()
-            seeds = None
-            index = None
-            if os.path.exists(label_path):
-                labels_df = pd.read_csv(label_path, header=None, sep="\s+")
-                labels_df[0] = np.array([CLASS_NAMES.index(n) for n in labels_df[0]])
-                seeds = torch.zeros(len(CLASS_NAMES)+1, 192, 640)
-                w, h = (640, 192)
-                w1, h1 = img.size
-                bbox = labels_df.iloc[:, 4:8].values
-                index = np.zeros((bbox.shape[0], 3))
-                index[:, 0] = labels_df[0].values
-                index[:, 2] = (bbox[:,0]+bbox[:,2])/2/w1*w
-                index[:, 1] = (bbox[:,1]+bbox[:,3])/2/h1*h
-                index = np.round(index).astype(int)
-                for img_class, y, x in index:
-                    seeds[img_class,y-5:y+5,x-5:x+5] = 1
-                seeds[len(CLASS_NAMES)] = 0.5
-        else:
+        label_path = self.label_files[file_index % len(self.img_files)].rstrip()
+        seeds = None
+        index = None
+        if os.path.exists(label_path):
+            labels_df = pd.read_csv(label_path, header=None, sep="\s+")
+            labels_df[0] = np.array([CLASS_NAMES.index(n) for n in labels_df[0]])
             seeds = torch.zeros(len(CLASS_NAMES)+1, 192, 640)
+            w, h = (640, 192)
+            w1, h1 = img.size
+            bbox = labels_df.iloc[:, 4:8].values
+            index = np.zeros((bbox.shape[0], 3))
+            index[:, 0] = labels_df[0].values
+            index[:, 2] = (bbox[:,0]+bbox[:,2])/2/w1*w
+            index[:, 1] = (bbox[:,1]+bbox[:,3])/2/h1*h
+            index = np.round(index).astype(int)
+            for img_class, y, x in index:
+                seeds[img_class,y-5:y+5,x-5:x+5] = 1
             seeds[len(CLASS_NAMES)] = 0.5
         
-        print(use_pair, img_path)
-        return input_img, seeds
+        img_pair = torch.cat([input_img_left, input_img_right], dim=0)
+        return img_pair, seeds
 
     def __len__(self):
         return 2*len(self.img_files)
